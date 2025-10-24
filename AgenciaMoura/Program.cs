48200caafdf3f4d50fd136b27098bf697419b8ea
FELIPE GOMES DE MENESES <52611156808@info.local>
2025-10-24 16:36:58 -0300

    Adicioneis atividades

---
+++ AgenciaMoura/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+
+string[] nomes = new string[10];
+// int[] idades = new int[3];
+float[] Saldo = new float[10];
+int totalClientes = 0;
+int opcao;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"==== Sistema Bancario ====");
+    Console.WriteLine($"1) Cadastrar Clientes");
+    Console.WriteLine($"2) Depositar");
+    Console.WriteLine($"3) Sacar");
+    Console.WriteLine($"4) Tranferir");
+    Console.WriteLine($"5) Listar Clientes");
+    Console.WriteLine($"0) Sair");
+    Console.WriteLine($"Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 0:
+
+            break;
+
+        case 1:
+            CadastrarCliente();
+            break;
+
+        case 2:
+            Depositar();
+            break;
+
+        case 3:
+            Sacar();
+            break;
+
+        case 4:
+            Tranferir();
+            break;
+
+        case 5:
+            ListarClientes();
+            break;
+        default:
+            break;  
+         
+}
+
+    Console.WriteLine($"Pressione <Enter> para continuar...");
+    Console.ReadLine();
+
+} while (opcao != 0);
+
+
+
+
+void CadastrarCliente()
+{
+    // Console.WriteLine($"Funcao cadastrar cliente em deselvolvimento");
+
+    if (totalClientes >= 10)
+    {
+        Console.WriteLine($"Limite de cadastros atingido");
+        return;
+    }
+    
+     Console.WriteLine($"Digite o nome do Cliente");
+    nomes[totalClientes] = Console.ReadLine();
+
+    Console.WriteLine($"Digite o saldo de  {nomes[totalClientes]}");
+    Saldo[totalClientes] = float.Parse(Console.ReadLine());
+    totalClientes++;
+    Console.WriteLine($"Aluno cadastrado com sucesso!");
+}
+
+void ListarClientes()
+{
+    Console.WriteLine($"Funcao listar cliente em deselvolvimento");
+}
+
+void Depositar()
+{
+    Console.WriteLine($"Funcao Depositar em deselvolvimento");
+}
+
+void Tranferir()
+{
+    Console.WriteLine($"Funcao Tranferir deselvolvimento");
+}
+
+void Sacar()
+{
+ Console.WriteLine($"Funcao Sacar em deselvolvimento");
+}
--- CalculadoraCompleta/obj/Debug/net9.0/CalculadoraCompleta.AssemblyInfo.cs
+++ CalculadoraCompleta/obj/Debug/net9.0/CalculadoraCompleta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraCompleta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2ecb5131f670ddb68487d1e6dd024e1b7ed4f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e5fc270ecbae96783ca58416c6c07dce80646b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraCompleta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraCompleta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
